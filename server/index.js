ef49aa0a22bb5073900331429f5a28d684002fbd
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-21 21:19:46 +0530

    Adding Routes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import Connection from './database/db.js';
-import dotenv from 'dotenv'
+import express from "express";
+import Connection from "./database/db.js";
+import dotenv from "dotenv";
+import router from "./Routes/route.js";
 
-
-const app=express();
+const app = express();
 dotenv.config();
-const PORT=8000;
-const username=process.env.DB_USERNAME;
-const password=process.env.DB_PASSWORD;
+const PORT = 8000;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 
+// ROuting
+app.use("/", router);
 
+Connection(username, password);
 
-
-Connection(username,password);
-
-app.listen(PORT,()=>console.log(`SErver is Runnning on ${PORT}`));
+app.listen(PORT, () => console.log(`SErver is Runnning on ${PORT}`));
