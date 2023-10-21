5f3291ad33d00532bd191de6d39e93e139541222
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-21 21:38:45 +0530

    Controller file for api callack

---
+++ server/controller/user-controller.js
@@ -0,0 +1,6 @@
+// this file contain all call back function
+
+// backend se frontend pe bhejne ke liye response ka used krte h
+export const addUser = (request, response) => {
+  console.log("Hello");
+};
