416948dddc1ed24d3445bd09ef5bc62b6b453114
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-24 22:33:13 +0530

    CHANGES

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,13 +1,12 @@
 // this file contain all call back function
 
-import { request } from "express";
-
 // backend se frontend pe bhejne ke liye response ka used krte h
+//request me wo sab aata h jo api ke saath front end se bheja jata hai...
+//Post APi ke saath Body Bheja jata hai...
+//express ka server bydefault post api ko handle nhi karta uske ke liyee post api ki body hoti h usko parsse nhi kr paata.
+//uske liye ek pakage ko instaal krna padta hai "npm i body-parser"
+//jo bhi front end se body aayegi usko validate karna padta h ki front end aa kya rha h...validation ke ek schema bana padta h..
 export const addUser = (request, response) => {
-  console.log("Hello");
+  const user = request.body;
+  console.log(user);
 };
-
-
-export const addUSers(request,response)=>{
-  //write main code
-}
