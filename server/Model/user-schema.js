bef6c70699055ca272270414164e9313f70650a1
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-24 22:33:37 +0530

    UPDATE

---
+++ server/Model/user-schema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: String,
+  username: String,
+  email: String,
+  phone: String,
+});
+
+//uper ke Schema ko databse me stor krne keliyeee....uske liye ek model/table banana padta h
+//mode(user ye ek table h..mongo db me users ke naam se ek table aagyi hogi mongdb bydefalut s laga deta h collection me.. )
+const user = mongoose.model("user", userSchema);
+export default user;
