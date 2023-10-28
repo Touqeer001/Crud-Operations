c5fc17d970cff3220198610c3010652a62dece70
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-28 20:53:03 +0530

    chnages

--- server/Model/user-schema.js
+++ server/Model/user-schema.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
 });
 
 //uper ke Schema ko databse me stor krne keliyeee....uske liye ek model/table banana padta h
-//mode(user ye ek table h..mongo db me users ke naam se ek table aagyi hogi mongdb bydefalut s laga deta h collection me.. )
+//mode(user ye ek table h..mongo db me users ke naam se ek table aagyi hogi mongdb bydefalut s laga deta h collection me.... )
 const user = mongoose.model("user", userSchema);
 
 export default user;
