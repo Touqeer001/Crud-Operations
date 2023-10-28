c8671fa48e7003422922f403300e6d490d0f29ea
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-28 21:43:47 +0530

    update

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ import dotenv from "dotenv";
 import router from "./Routes/route.js";
 // import bodyParser from "body-parser";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+
 dotenv.config();
+app.use(cors());
+
 const PORT = 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
