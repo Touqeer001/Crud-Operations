3eb89058206af3ecf022e3e5bf00a3ef16efb33d
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-28 21:44:07 +0530

    update

--- server/Routes/route.js
+++ server/Routes/route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUser } from "../controller/user-controller";
+import { addUser } from "../controller/user-controller.js";
 
 const router = express.Router();
 
