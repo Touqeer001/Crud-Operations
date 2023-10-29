6b29597f5dc07116a1521b5578091db19f86aa03
Touqeer Ansari <touqeeransari001@gmail.com>
2023-10-29 19:22:42 +0530

    Updated

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -7,7 +7,7 @@
 //Post APi ke saath Body Bheja jata hai...
 //express ka server bydefault post api ko handle nhi karta uske ke liyee post api ki body hoti h usko parsse nhi kr paata.
 //uske liye ek pakage ko instaal krna padta hai "npm i body-parser"
-//jo bhi front end se body aayegi usko validate karna padta h ki front end aa kya rha h...validation ke ek schema bana padta h..
+//jo bhi front end se body aayegi usko validate karna padta h ki front end aa kya rha h...validation ke ek schema bana padta h....
 export const addUser = (request, response) => {
   const user = request.body;
   console.log(user);
