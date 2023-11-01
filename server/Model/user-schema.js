522dac2209579d3d5022c0e40ffd9e28ff1ef775
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-01 12:42:27 +0530

    updated

--- server/Model/user-schema.js
+++ server/Model/user-schema.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-
+import autoIncrement from "mongoose-auto-increment";
 const userSchema = mongoose.Schema({
   name: String,
   username: String,
   email: String,
   phone: String,
 });
+//autoincrement se
+// autoIncrement.initialize(mongoose.connection);
+// userSchema.plugin(autoIncrement.plugin, "user");
 
 //uper ke Schema ko databse me stor krne keliyeee....uske liye ek model/table banana padta h....
 //mode(user ye ek table h..mongo db me users ke naam se ek table aagyi hogi mongdb bydefalut s laga deta h collection me.... )
