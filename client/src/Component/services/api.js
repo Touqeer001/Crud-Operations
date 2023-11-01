aefee65331d79b54ec8696986f192e00a5ac40dd
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-01 12:41:54 +0530

    Updated

--- client/src/Component/services/api.js
+++ client/src/Component/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const Url = "http://localhost:8000";
 export const addUser = async (data) => {
   try {
-    await axios.post(`${Url}/add`, data);
+    return await axios.post(`${Url}/add`, data);
   } catch (error) {
     console.log("Eroor while calling the Api addUser", error);
   }
