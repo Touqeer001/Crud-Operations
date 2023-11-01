974d4384f95e208857a6d20ce01e4cccc80809e8
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-01 20:26:36 +0530

    Update

--- client/src/Component/AllUser.jsx
+++ client/src/Component/AllUser.jsx
@@ -1,14 +1,22 @@
-import { useEffect } from "react"; //useEffect ye 3 chizo se mil kr bana h ek "componentDidMount,ekComponentUpdate,or Ek ComponentMount."
-import { Table, TableHead, TableCell, TableRow } from "@mui/material";
+import { useEffect, useState } from "react"; //useEffect ye 3 chizo se mil kr bana h ek "componentDidMount,ekComponentUpdate,or Ek ComponentMount."
+import {
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+} from "@mui/material";
 import { getUsers } from "../Component/services/api.js";
 
 const AllUser = () => {
+  const [users, setUsers] = useState([]); //Api ka sara data users me store ho gya h
   useEffect(() => {
     getAllUsers();
   }, []); //ye wali condition componentdidMount ki hoti h Empty array is liye pass kiye h ki ek baar call ho
 
   const getAllUsers = async () => {
-    await getUsers(); //ye api h jo aa rhi h api.js file se.
+    let response = await getUsers(); //ye api h jo aa rhi h api.js file se.
+    setUsers(response.data);
   };
 
   return (
@@ -16,13 +24,24 @@ const AllUser = () => {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Id</TableCell>
+            {/* <TableCell>Id</TableCell> */}
             <TableCell>Name</TableCell>
             <TableCell>UserName</TableCell>
             <TableCell>Email</TableCell>
             <TableCell>Phone</TableCell>
           </TableRow>
         </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow>
+              {/* <TableCell>{user._id}</TableCell> */}
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </Table>
     </>
   );
