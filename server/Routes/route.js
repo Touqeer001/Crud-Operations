9df2e169dabc2894ae6ac9cee2564cf78a14717b
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-02 22:34:05 +0530

    update

--- server/Routes/route.js
+++ server/Routes/route.js
@@ -1,11 +1,11 @@
 //All Routes Calling..
 
 import express from "express";
-import { addUser, getUsers } from "../controller/user-controller.js";
+import { addUser, getUsers, getUSer } from "../controller/user-controller.js";
 
 const router = express.Router();
 
 router.post("/add", addUser);
 router.get("/all", getUsers);
-
+router.get("/:id", getUSer);
 export default router;
