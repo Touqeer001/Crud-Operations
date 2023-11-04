e377c68953f8e48ce9b42dc2f8752d0956a5e40c
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-04 22:37:02 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import AllUser from "./Component/AllUser";
 import CrudOperation from "./Component/CrudOpearion";
 import EditUser from "./Component/Edit";
 import NavBar from "./Component/NavBar";
-// import { BrowserRouter, Routes, Route }  from "react-router-dom";
+// import { BrowserRouter, Routes, Route }  from "react-router-dom"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
