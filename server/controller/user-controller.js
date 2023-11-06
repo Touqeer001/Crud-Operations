792cf67944ca0e0aebd6c022df2e1560df2aab98
Touqeer Ansari <touqeeransari001@gmail.com>
2023-11-06 19:40:18 +0530

    update file

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -5,7 +5,7 @@
 // backend se frontend pe bhejne ke liye response ka used krte h
 //request me wo sab aata h jo api ke saath front end se bheja jata hai...
 //Post APi ke saath Body Bheja jata hai...
-//express ka server bydefault post api ko handle nhi karta uske ke liyee post api ki body hoti h usko parsse nhi kr paata.
+//express ka server bydefault post api ko handle nhi karta uske ke liyee post api ki body hoti h usko paarsse nhi kr paata.
 //uske liye ek pakage ko instaal krna padta hai "npm i body-parser"
 //jo bhi front end se body aayegi usko validate karna padta h ki front end aa kya rha h...validation ke ek schema bana padta h..
 
@@ -13,10 +13,10 @@ import User from "../Model/user-schema.js";
 
 export const addUser = async (request, response) => {
   const user = request.body;
-  //ager succesfully user validate krta h to use data base save karana padegaa.
+  //ager succesfully user validate krta h to use data base save karana padegaa..
   const newUser = new User(user);
   try {
-    await newUser.save(); //save ek mongodb ka function h jo saaredatabase ko savr kr degaa..
+    await newUser.save(); //save ek mongodb ka function h jo saara database ko save kr degaa.
     response.status(201).json(newUser);
   } catch (error) {
     // ager data ko save krne me unsuccesful rha to
